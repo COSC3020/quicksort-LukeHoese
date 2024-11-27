2df677531afc5281ef68b59998bee8af76d80572
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-26 23:13:51 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
 function quicksort(array) {
+    var stack = [];
+
+    stack.push(0);
+    stack.push(array.length - 1);
+
+    while (stack.length > 0) {
+        var high = stack.pop();
+        var low = stack.pop();
+
+        var index = partition(array, low, high);
+
+        if (index - 1 > left) {
+            stack.push(low);
+            stack.push(index - 1);
+        }
+
+        if (index + 1 < right) {
+            stack.push(index + 1);
+            stack.push(right);
+        }
+    }
+
     return array;
+}
+
+function partition(array, low, high) {
+    var pivot = array[high];
+    var x = low - 1;
+
+    for (y = low; y < high; y++) {
+        if (array[y] <= pivot) {
+            x++
+            [array[x], array[y]] = [array[y], array[x]];
+        }
+    }
+
+    [array[x + 1], array[high]] = [array[right], array[x + 1]];
+
+    return x + 1;
 }
